fea6c19deb7ff3bd2de3ab502fef9433b6be89a2
Wickramarathnampnp <129901580+Wickramarathnampnp@users.noreply.github.com>
2024-08-27 01:01:21 +0530

    Add new DTO

---
+++ team4.LTS.Core/Model/DTO/ReservetionRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team4.LTS.Core.Model.DTO
+{
+	public  class ReservetionRequestDTO
+	{
+		[Required]
+		public int RequstedEquipmentId { get; set; }
+		[Required]
+		public int ReservedBy { get; set; }
+
+	}
+}
