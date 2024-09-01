6ef507b16ab5700c739334ce438a56415284e46c
Sharada Shehan <sharadashehan6@gmail.com>
2024-09-02 03:08:45 +0530

    Unit test for get user role API

---
+++ IMS.Tests/Utilities/HttpContextHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMS.Tests.Utilities
+{
+    public static class HttpContextHelper
+    {
+        public static void MockHttpContext(ControllerBase controller, string authorizationHeader)
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Authorization"] = authorizationHeader;
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+        }
+    }
+}
