cfea8aa808d84f02bd8d01faa091c0894759e3d2
Sharada Shehan <sharadashehan6@gmail.com>
2024-09-09 15:09:56 +0530

    test api for base url

---
+++ IMS.Presentation/Controllers/TestControllers.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
+
+namespace IMS.Presentation.Controllers
+{
+    [Route("")]
+	[ApiController]
+	public class TestControllers : ControllerBase
+    {
+
+		[HttpGet("")]
+        public IActionResult RedirectToTestAPI()
+        {
+            return RedirectToAction(nameof(TestAPI));
+        }
+
+
+        [HttpGet("api/test")]
+        public async Task<IActionResult> TestAPI()
+        {
+            try
+            {
+                var welcomeText = new
+                {
+                    message = "Welcome to the Test API",
+                    status = "success",
+                    system = new {
+                        version = "1.0.0",
+                        name = "Inventory Management System for Computer Labourataries",
+                        description = "This is a test API for the Inventory Management System for Computer Labourataries",
+                        author = "Group 22"
+                    }
+                };
+                return Ok(welcomeText);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
